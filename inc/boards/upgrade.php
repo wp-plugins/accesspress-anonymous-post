94cba054623e7e203374b72f0240909d2d95714a
Access Keys <Access Keys@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-08 06:15:50 +0000

    Added Serbian Language and released 2.1.4 version
    git-svn-id: https://plugins.svn.wordpress.org/accesspress-anonymous-post/tags/2.1.4@1084818 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/boards/upgrade.php
@@ -0,0 +1,28 @@
+<div class="ap-tabs-board" id="board-upgrade-settings" style="display: none;">
+<h2><?php _e('Upgrade','anonymous-post');?></h2>
+	<div class="ap-tab-wrapper">
+		<p><strong>For more features upgrade to PRO version. </strong><br />
+		<strong>Regular Licence:</strong> Use, by you or one client, in a single end product which end users are not charged for. The total price includes the item price and a buyer fee. <br />
+		<strong>Price:</strong> $24<br /><br />
+        <strong>Extended Licence:</strong> Use, by you or one client, in a single end product which end users can be charged for. The total price includes the item price and a buyer fee.  <br />
+		<strong>Price:</strong> $120<br /><br />
+        Upgrades: <strong>Free lifetime upgrades</strong> </p>
+		<div class="upgrade"><a href="http://accesspressthemes.com/wordpress-plugins/accesspress-anonymous-post-premium/" target="_blank">Upgrade now! </a></div>
+	
+		<h3 class="feature-title">Pro features </h3>
+        <img src="<?php echo AP_IMAGE_DIR.'/anonymous-post-pro.jpg';?>" width="100%"/>
+		<!--<ul class="features">
+			<li>Custom Post Types and Taxonomy</li>
+			<li>Add more notification emails upto 3</li>
+			<li>Built in form templates</li>
+			<li>Specify form fields</li>
+			<li>Specify optional/mandatory form fields</li>
+			<li>Specify custom mandatory form field message</li>
+			<li>Google captcha</li>
+			<li>Form designer</li>
+			<li>Media upload</li>
+		</ul>-->
+
+
+	</div>
+</div>
