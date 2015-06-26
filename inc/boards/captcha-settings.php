e1a0cc9231004a76f09eb2d1730cdedcae00d16b
Access Keys <Access Keys@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-26 04:34:50 +0000

    AccessPress Anonymous Post - Done some modifications on default settings
    git-svn-id: https://plugins.svn.wordpress.org/accesspress-anonymous-post/tags/2.3.2@1187915 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/boards/captcha-settings.php
@@ -0,0 +1,24 @@
+<div class="ap-tabs-board" id="board-captcha-settings" style="display: none;">
+  <h2><?php _e('Captcha Settings','anonymous-post');?></h2>
+  <div class="ap-tab-wrapper">
+    <div class="ap-option-wrapper">
+      <div class="ap-option-field">
+       <div class="ap-checkbox-form"><label class="ap-label-wrapper"><input type="checkbox" name="captcha_settings" value="1" <?php if($ap_settings['captcha_settings']=='1'){?>checked="checked"<?php }?> class="ap-captcha-selector"/><span><?php _e('Show Mathmatical Captcha','anonymous-post')?></span></label></div>
+     </div>
+   </div>
+   <div class="captcha-fields ap-human-captcha-fields">
+    <div class="ap-option-wrapper">
+      <label><?php _e('Mathmatical Captcha Label','anonymous-post')?></label>
+      <div class="ap-option-field">
+        <input type="text" name="math_captcha_label" value="<?php echo esc_attr($ap_settings['math_captcha_label']);?>"/>
+      </div>
+    </div>
+    <div class="ap-option-wrapper">
+      <label><?php _e('Mathmatical Captcha Error Message','anonymous-post')?></label>
+      <div class="ap-option-field">
+        <input type="text" name="math_captcha_error_message" value="<?php echo esc_attr($ap_settings['math_captcha_error_message']);?>"/>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
